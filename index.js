73318de3aa378d988f872ffec71c29488dae932d
stackblaze <119256201+stackblaze@users.noreply.github.com>
2024-08-08 17:40:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello 89')
+  response.send('Hello 90')
 })
 
 app.listen(app.get('port'), function() {
